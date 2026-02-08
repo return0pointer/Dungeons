b5293b5c1a3dd6a5521a75cf620ce453ae6ee06b
MihailStolyar <FairHunter11>
2026-02-08 16:57:56 +0200

    switch to 5.7.1

--- Source/Dungeons.Target.cs
+++ Source/Dungeons.Target.cs
@@ -8,8 +8,8 @@ public class DungeonsTarget : TargetRules
 	public DungeonsTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V4;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_3;
+		DefaultBuildSettings = BuildSettingsVersion.V6;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
 		ExtraModuleNames.Add("Dungeons");
 	}
 }
--- Source/DungeonsEditor.Target.cs
+++ Source/DungeonsEditor.Target.cs
@@ -8,8 +8,8 @@ public class DungeonsEditorTarget : TargetRules
 	public DungeonsEditorTarget( TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V4;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_3;
+		DefaultBuildSettings = BuildSettingsVersion.V6;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
 		ExtraModuleNames.Add("Dungeons");
 	}
 }
